29eb43ccc24eb9e327248593c880201da3f44188
marceloalima <marcelo.alima@ifce.edu.br>
2024-02-21 11:13:14 -0300

    added new methods

--- internal/domain/repository/client_repository.go
+++ internal/domain/repository/client_repository.go
@@ -3,6 +3,7 @@ package repository
 import "github.com/marcelospfcufc/rinha_backend_2024/internal/domain/entity"
 
 type ClientRepository interface {
+	HasClientById(clientId entity.Id) bool
 	Create(client entity.Client) (entity.Client, error)
 	GetById(clientId entity.Id) (entity.Client, error)
 }
