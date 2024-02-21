fc124ea002e1575a213eca5efab9c6614e0dd07b
marceloalima <marcelo.alima@ifce.edu.br>
2024-02-21 11:14:40 -0300

    try to create a concept of enum to represent OrderBy param

--- internal/domain/repository/custom_type.go
+++ internal/domain/repository/custom_type.go
@@ -8,3 +8,18 @@ type ValueType int32
 type OperationType string
 type RealizedInType *time.Time
 type DescriptionType string
+
+type OrderBy int
+
+const (
+	Asc OrderBy = iota
+	Desc
+)
+
+func (order OrderBy) String() string {
+	orders := [...]string{"asc", "desc"}
+	if order < Asc || order > Desc {
+		panic("OrderBy with invalid value")
+	}
+	return orders[order]
+}
