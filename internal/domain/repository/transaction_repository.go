5dbbbc8d44c98c478b8d7f6393a0248ddd47af9a
marceloalima <marcelo.alima@ifce.edu.br>
2024-02-21 11:11:12 -0300

    added calculate balance in repository to try get better performance

--- internal/domain/repository/transaction_repository.go
+++ internal/domain/repository/transaction_repository.go
@@ -1,17 +1,16 @@
 package repository
 
-import "github.com/marcelospfcufc/rinha_backend_2024/internal/domain/entity"
+import (
+	"github.com/marcelospfcufc/rinha_backend_2024/internal/domain/entity"
+)
 
-type TransactionRepositoryData struct {
-	TransactionId TransactionIdType
-	Value         ValueType
-	Operation     OperationType
-	RealizedIn    RealizedInType
-	Description   DescriptionType
-	ClientId      any
+type SummaryBalanceRepositoryData struct {
+	Balance int64
+	Credit  int64
 }
 
 type TransactionRepository interface {
 	Create(clientId entity.Id, transaction entity.Transaction) (entity.Transaction, error)
-	GetAllByUser(clientId entity.Id) ([]entity.Transaction, error)
+	GetAllByUser(clientId entity.Id, limit int, orderBy OrderBy) ([]entity.Transaction, error)
+	SummaryBalanceByClient(clientId entity.Id) (SummaryBalanceRepositoryData, error)
 }
